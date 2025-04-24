d35a8887fe2925eeca42e9f4dd266fa0c38afb9a
rohan-tridhyatech <rohanmovaliya.tridhyatech@gmail.com>
2025-04-24 16:49:04 +0530

    Add Changes

--- UserManagement/User/admin.py
+++ UserManagement/User/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("bio",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("bio",)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- UserManagement/User/models.py
+++ UserManagement/User/models.py
@@ -2,5 +2,4 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Add any additional fields here
     bio = models.TextField(blank=True, null=True)
